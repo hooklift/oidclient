c9a14397d7b6d91220df8b0aab4ad9532c7405ed
Camilo Aguilar <camilo.aguilar@gmail.com>
2019-02-19 00:09:58 -0500

    Example godoc formatting

--- example_test.go
+++ example_test.go
@@ -109,7 +109,7 @@ func Example_mobile() {
 	httpClient, err := provider.HTTPClient(ctx, tokens)
 }
 
-func ExampleExamples_web() {
+func Example_web() {
 	ctx := context.Background()
 	provider, err := oidclient.New(ctx, "https://id.hooklift.io", []oidclient.ProviderOption{
 		oidclient.ClientID("blah"),
